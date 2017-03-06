0d6762cca3d16ad42c8f83098a5bb0e9bc38e8fd
micawhitby <micawhitby@gmail.com>
2017-03-06 12:01:43 +0000

    first unit test passing, account initializes with a balance of 0

---
+++ lib/account.rb
@@ -0,0 +1,9 @@
+class Account
+
+  def initialize
+    @balance = 0
+  end
+
+attr_accessor :balance
+
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,7 @@ describe Account do
   subject(:account) { described_class.new }
 
   it 'initalilizes with an account balance of 0' do
-    expect(account.balance).to equal(0)
+    expect(account.balance).to eq(0)
   end
 
 
